c67a7ff3f6b90c43b3b1ea94200264ceae1c10f2
Haï~ <hai-vr@users.noreply.github.com>
2025-07-10 23:02:43 +0200

    Add Program.cs build.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿using System.IO;
+using Newtonsoft.Json;
+
+object ourIndex = new
+{
+    title = "Written by Program.cs",
+    generated = DateTime.UtcNow
+};
+
+File.WriteAllText("output/index.json", JsonConvert.SerializeObject(ourIndex, Formatting.Indented));
