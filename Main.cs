72aac25c006701cfc7b62105a5e800d7ad04df4d
Haï~ <hai-vr@users.noreply.github.com>
2025-07-11 20:24:02 +0200

    Split responsibilities into different files.

---
+++ Main.cs
@@ -0,0 +1,71 @@
+﻿using System.Text;
+using Newtonsoft.Json;
+
+namespace Hai.PristineListing;
+
+internal class Program
+{
+    private readonly string _inputFile;
+    private readonly bool _includeDownloadCount;
+    
+    private readonly PLGatherer _gatherer;
+    private readonly PLOutputter _outputter;
+
+    public static async Task Main(string[] args)
+    {
+        string EnvVar(string var) => Environment.GetEnvironmentVariable(var);
+
+        try
+        {
+            var githubToken = EnvVar("IN__GITHUB_TOKEN");
+            if (string.IsNullOrWhiteSpace(githubToken)) throw new ArgumentException("IN__GITHUB_TOKEN env var contains nothing");
+
+            var excessiveMode = false;
+            var includeDownloadCount = false;
+
+            if (bool.TryParse(EnvVar("IN__EXCESSIVE_MODE"), out var doExcessiveMode)) excessiveMode = doExcessiveMode;
+            if (bool.TryParse(EnvVar("IN__INCLUDE_DOWNLOAD_COUNT"), out var doIncludeDownloadCount)) includeDownloadCount = doIncludeDownloadCount;
+
+            var inputFile = "input.json";
+            var outputFile = "index.json";
+
+            await new Program(githubToken, inputFile, $"output/{outputFile}", excessiveMode, includeDownloadCount).Run();
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine($"Error occurred: {e.Message}");
+            Console.WriteLine(e);
+            throw;
+        }
+    }
+
+    private Program(string githubToken, string inputFile, string outputFile, bool excessiveMode, bool includeDownloadCount)
+    {
+        _inputFile = inputFile;
+        _includeDownloadCount = includeDownloadCount;
+
+        _gatherer = new PLGatherer(githubToken, excessiveMode);
+        _outputter = new PLOutputter(outputFile);
+    }
+
+    private async Task Run()
+    {
+        Directory.CreateDirectory("output");
+        
+        var inputJson = await File.ReadAllTextAsync(_inputFile, Encoding.UTF8);
+        var input = JsonConvert.DeserializeObject<PLInput>(inputJson);
+        
+        var outputListing = await _gatherer.DownloadAndAggregate(input);
+        
+        foreach (var outputListingPackage in outputListing.packages.Values)
+        {
+            foreach (var version in outputListingPackage.versions.Values)
+            {
+                var description = version.description ?? version.displayName;
+                version.description = _includeDownloadCount ? $"{description} (Downloaded {version.downloadCount} times)" : description;
+            }
+        }
+
+        await _outputter.Write(outputListing);
+    }
+}
---
+++ Outputter.cs
@@ -0,0 +1,81 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Text;
+using Markdig;
+using Newtonsoft.Json;
+
+namespace Hai.PristineListing;
+
+internal class PLOutputter
+{
+    private readonly string _outputIndexJson;
+
+    internal PLOutputter(string outputIndexJson)
+    {
+        _outputIndexJson = outputIndexJson;
+    }
+
+    internal async Task Write(PLOutputListing outputListing)
+    {
+        await Task.WhenAll(new[] { CreateListing(outputListing), CreateWebpage(outputListing) });
+    }
+
+    private async Task CreateListing(PLOutputListing outputListing)
+    {
+        var outputJson = JsonConvert.SerializeObject(outputListing, Formatting.Indented, new JsonSerializerSettings
+        {
+            NullValueHandling = NullValueHandling.Ignore
+        });
+
+        await File.WriteAllTextAsync(_outputIndexJson, outputJson, Encoding.UTF8);
+    }
+
+    [SuppressMessage("ReSharper", "MethodHasAsyncOverload")]
+    private async Task CreateWebpage(PLOutputListing outputListing)
+    {
+        var sw = new StringWriter();
+        sw.WriteLine($"# {outputListing.id}");
+        sw.WriteLine("");
+        foreach (var package in outputListing.packages)
+        {
+            var versions = package.Value.versions;
+            sw.WriteLine($"## {package.Key}");
+            sw.WriteLine("");
+
+            var firstVersion = versions.Values.First();
+            sw.WriteLine($"- displayName: {firstVersion.displayName}");
+            sw.WriteLine($"- description: {firstVersion.description}");
+            sw.WriteLine($"- totalDownloadCount: {versions.Values.Select(version => version.downloadCount).Sum()}");
+            if (firstVersion.changelogUrl != null) sw.WriteLine($"- changelogUrl: {firstVersion.changelogUrl}");
+            if (firstVersion.documentationUrl != null) sw.WriteLine($"- documentationUrl: {firstVersion.documentationUrl}");
+            if (firstVersion.unity != null) sw.WriteLine($"- unity: {firstVersion.unity}");
+            if (firstVersion.vrchatVersion != null) sw.WriteLine($"- vrchatVersion: {firstVersion.vrchatVersion}");
+            if (firstVersion.dependencies != null && firstVersion.dependencies.Count > 0)
+            {
+                sw.WriteLine("- dependencies:");
+                foreach (var dep in firstVersion.dependencies)
+                {
+                    sw.WriteLine($"  - {dep.Key} : {dep.Value}");
+                }
+            }
+            if (firstVersion.vpmDependencies != null && firstVersion.vpmDependencies.Count > 0)
+            {
+                sw.WriteLine("- vpmDependencies:");
+                foreach (var dep in firstVersion.vpmDependencies)
+                {
+                    sw.WriteLine($"  - {dep.Key} : {dep.Value}");
+                }
+            }
+            sw.WriteLine($"- versions:");
+            foreach (var version in versions.Values)
+            {
+                sw.WriteLine($"  - {version.version} -> {version.downloadCount}");
+            }
+            sw.WriteLine("");
+        }
+
+        var markdown = sw.ToString();
+        var html = Markdown.ToHtml(markdown);
+        await File.WriteAllTextAsync("output/list.md", markdown, Encoding.UTF8);
+        await File.WriteAllTextAsync("output/index.html", html, Encoding.UTF8);
+    }
+}
